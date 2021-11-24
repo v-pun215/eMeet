c12230225fd747d2b291018b2ad2fea9b4874f0d
vihaanpundir <67716965+vihaanpundir@users.noreply.github.com>
2021-11-24 11:11:04 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function hm {
+  document.getElementById("home").style.display = "block";
+  document.getElementById("meet").style.display = "none";
+}
+function mt {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("meet").style.display = "block";
+}
